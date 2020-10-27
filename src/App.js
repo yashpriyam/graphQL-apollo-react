f957f0b6b876b2340a5d88b5c237f5fb35306508
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-10-27 19:11:14 +0530

    add apollo, create client and use queries

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import AllFilesData from './components/allFileFolder.query'
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  
+  return <div className="App">Apollo GraphQL
+  <AllFilesData/>
+  </div>;
 }
 
 export default App;
